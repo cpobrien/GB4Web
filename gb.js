4bb52f8e8e59d6196e80acb899d659373018aab6
Connor O'Brien <connorpadraigobrien@gmail.com>
2018-01-07 22:10:49 -0800

    Create basic CPU class

--- gb.js
+++ gb.js
@@ -1,3 +1,29 @@
+function jp() {
+
+}
+
+class CPU {
+    constructor(rom) {
+        this.rom = rom;
+
+        this.pc = 0x100;
+        this.sp = 0xFFFE;
+        this.A = 0;
+        this.B = 0;
+        this.C = 0;
+        this.D = 0;
+        this.E = 0;
+        this.F = {
+            Z: 0,
+            N: 0,
+            H: 0,
+            C: 0,
+        };
+        this.H = 0;
+        this.L = 0;
+    }
+}
+
 class ROMFile {
     constructor(buffer) {
         this.rom = buffer;
@@ -7,5 +33,6 @@ class ROMFile {
 fetch('zelda.gb')
     .then(response => response.arrayBuffer())
     .then(arrayBuffer => {
-        var romFile = new ROMFile(new Uint8Array(arrayBuffer));
+        var rom = new ROMFile(new Uint8Array(arrayBuffer));
+        var cpu = new CPU(rom);
     });
