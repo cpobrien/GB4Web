8209569870778db32e679f91edcfb9d8f18bebc2
Connor O'Brien <connorpadraigobrien@gmail.com>
2018-01-07 22:36:57 -0800

    Implement routine for running z80 + basic jp

--- gb.js
+++ gb.js
@@ -1,5 +1,42 @@
-function jp() {
+var opcodeMap = [
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, jp, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+    nop, nop, nop, nop, nop, nop, nop, nop,
+];
 
+function nop(cpu) { return cpu.pc++ }
+
+function jp(cpu) {
+    cpu.pc = cpu.rom.read16(cpu.pc + 1);
 }
 
 class CPU {
@@ -8,6 +45,7 @@ class CPU {
 
         this.pc = 0x100;
         this.sp = 0xFFFE;
+
         this.A = 0;
         this.B = 0;
         this.C = 0;
@@ -22,11 +60,36 @@ class CPU {
         this.H = 0;
         this.L = 0;
     }
+
+    run() {
+        var nopCount = 0;
+        while (nopCount <= 1) {
+            this.printState();
+            var address = this.rom.read(this.pc);
+            var op = opcodeMap[address];
+            if (op == nop) nopCount++; // ew
+            op(this);
+        }
+    }
+
+    printState() {
+        console.log(`PC: 0x${this.pc.toHex()}`)
+    }
 }
 
 class ROMFile {
     constructor(buffer) {
         this.rom = buffer;
+    }
+
+    read(address) {
+        return this.rom[address];
+    }
+
+    read16(address) {
+        var lo = this.rom[address];
+        var hi = this.rom[address + 1];
+        return hi << 8 | lo;
     }
 }
 
@@ -34,5 +97,9 @@ fetch('zelda.gb')
     .then(response => response.arrayBuffer())
     .then(arrayBuffer => {
         var rom = new ROMFile(new Uint8Array(arrayBuffer));
-        var cpu = new CPU(rom);
+        new CPU(rom).run();
     });
+
+Number.prototype.toHex = function() {
+    return this.toString(16);
+};
