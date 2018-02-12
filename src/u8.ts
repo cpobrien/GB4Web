aab6a6c03b3acb99cfc3c32ff0b46f040bdcc50e
Connor O'Brien <connorpadraigobrien@gmail.com>
2018-02-12 01:10:10 -0800

    expand tests

--- src/u8.ts
+++ src/u8.ts
@@ -3,7 +3,12 @@ class U8 {
     constructor(val: number) { this.val = val & 0xFF; }
     add(byte: U8): U8 {
         const sum: number = this.val + byte.val;
-        return new U8(sum % 0xFF);
+        return new U8(sum % 0xF00);
+    }
+
+    subtract(byte: U8): U8 {
+        const difference: number = this.val - byte.val;
+        return new U8(difference % 0xFF);
     }
 }
 
--- tst/u8.test.ts
+++ tst/u8.test.ts
@@ -1,10 +1,28 @@
 import { expect } from 'chai';
 import { U8 } from "../src/u8";
 
-describe('Unsigned bytes are working', () => {
-  it('adds properly', () => {
-      const byte: U8 = new U8(255);
-      const rem: U8 = byte.add(new U8(1));
-      expect(rem.val).equal(1);
-  });
+describe('unsigned byte', () => {
+    describe('addition', () => {
+        it('happy path', () => {
+           const res: U8 = new U8(1).add(new U8(1));
+           expect(res.val).equal(2);
+        });
+
+        it('overflow', () => {
+            const res: U8 = new U8(255).add(new U8(1));
+            expect(res.val).equal(0);
+        });
+    });
+
+    describe('subtraction', () => {
+        it('happy path', () => {
+          const res: U8 = new U8(1).subtract(new U8(1));
+          expect(res.val).equal(0)
+        });
+
+        it('underflow', () => {
+            const res: U8 = new U8(0).subtract(new U8(1));
+            expect(res.val).equal(255);
+        })
+    })
 });
